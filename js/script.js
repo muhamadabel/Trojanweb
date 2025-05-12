e62d428b2175cd1629c4254cdcc842f2962ef226
muhamadabel <81030737+muhamadabel@users.noreply.github.com>
2025-05-12 08:43:07 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// togle klas aktif
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik luar sedbar
+const menu = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
